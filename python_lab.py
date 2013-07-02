fac56e8b3305b3e56b590ecfe788712d0e3fd228
Brandon Healy <mbh4k@hotmail.com>
2013-07-01 22:07:10 -0400

    Update to python_lab.py that finishes the assignment and seems to upload correctly.

--- python_lab.py
+++ python_lab.py
@@ -54,60 +54,60 @@ zero_sum_list = [ (i,j,k) for i in S for j in S for k in S if sum([i,j,k]) == 0 
 exclude_zero_list = [ (i,j,k) for i in S for j in S for k in S if (sum([i,j,k]) == 0) and ([i,j,k] != [0,0,0]) ]
 
 ## Task 16
-first_of_tuples_list = ...
+first_of_tuples_list = [ (i,j,k) for i in S for j in S for k in S if (sum([i,j,k]) == 0) and ([i,j,k] != [0,0,0]) ][0]
 
 ## Task 17
-L1 = [...] # <-- want len(L1) != len(list(set(L1)))
-L2 = [...] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
+L1 = [1,2,2,3,4,5] # <-- want len(L1) != len(list(set(L1)))
+L2 = [2,1,5,4,3] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
 
 ## Task 18
-odd_num_list_range = {...}
+odd_num_list_range = {x for x in range(0,100) if x % 2 == 1}
 
 ## Task 19
 L = ['A','B','C','D','E']
-range_and_zip = ...
+range_and_zip = list(zip(range(0,5),L))
 
 ## Task 20
-list_sum_zip = [...]
+list_sum_zip = [x+y for (x,y) in zip([10,25,40],[1,15,20])]
 
 ## Task 21
 dlist = [{'James':'Sean', 'director':'Terence'}, {'James':'Roger', 'director':'Lewis'}, {'James':'Pierce', 'director':'Roger'}]
 k = 'James'
-value_list = [...]
+value_list = [dlist[i][k] for i in range(dlist.__len__())]
 
 ## Task 22
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
 k = 'Bilbo'
-value_list_modified_1 = [...] # <-- Use the same expression here
+value_list_modified_1 = [dlist[i][k] if k in dlist[i] else "NOT PRESENT" for i in range(dlist.__len__())] # <-- Use the same expression here
 k = 'Frodo'
-value_list_modified_2 = [...] # <-- as you do here
+value_list_modified_2 = [dlist[i][k] if k in dlist[i] else "NOT PRESENT" for i in range(dlist.__len__())] # <-- as you do here
 
 ## Task 23
-square_dict = {...}
+square_dict = {x:x**2 for x in range(0,100)}
 
 ## Task 24
 D = {'red','white','blue'}
-identity_dict = {...}
+identity_dict = {x:x for x in D}
 
 ## Task 25
 base = 10
 digits = set(range(10))
-representation_dict = {...}
+representation_dict = {x*(base**2)+y*base+z:[x,y,z] for x in digits for y in digits for z in digits}
 
 ## Task 26
 d = {0:1000.0, 1:1200.50, 2:990}
 names = ['Larry', 'Curly', 'Moe']
-listdict2dict = { ... }
+listdict2dict = { names[i]:d[i] for i in range(L.__len__()) if i in d }
 
 ## Task 27
-def nextInts(L): return [ ... ]
+def nextInts(L): return [ x+1 for x in L ]
 
 ## Task 28
-def cubes(L): return [ ... ] 
+def cubes(L): return [ x**3 for x in L ] 
 
 ## Task 29
-def dict2list(dct, keylist): return [ ... ]
+def dict2list(dct, keylist): return [ dct[keylist[i]] for i in range(keylist.__len__()) ]
 
 ## Task 30 
-def list2dict(L, keylist): return { ... } 
+def list2dict(L, keylist): return { keylist[i]:L[i] for i in range(L.__len__()) } 
 
