32081c14f8b20a086c68161e8f3a521348ac96eb
Brandon <brandon@Dianas-MacBook-Air.local>
2013-09-09 09:29:50 -0400

    Finished the hw0.py assignment.

--- week0/hw0.py
+++ week0/hw0.py
@@ -15,45 +15,66 @@ def myLists(L): return [list(range(1,x+1)) for x in L]
 
 
 ## Problem 3
-def myFunctionComposition(f, g): pass
+def myFunctionComposition(f, g): return {x:g[f[x]] for x in f}
 
 
 ## Problem 4
 # Please only enter your numerical solution.
 
-complex_addition_a = ... 
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = (5+3j) 
+complex_addition_b = (0+1j)
+complex_addition_c = (-1+0.001j)
+complex_addition_d = (0.001+9j)
 
 
 
 ## Problem 5
-GF2_sum_1 = ...
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
 
 ## Problem 6
-def mySum(L): pass
+def mySum(L): 
+    sumval = 0
+    for currval in L:
+        sumval += currval
+    return sumval
 
 
 
 ## Problem 7
-def myProduct(L): pass
+def myProduct(L):
+    multval = 1
+    for currval in L:
+        multval *= currval
+    return multval
 
 
 
 ## Problem 8
-def myMin(L): pass
+def myMin(L):
+    minval = L[0]
+    for currval in L:
+        if currval < minval:
+            minval = currval
+    return minval
 
 
 
 ## Problem 9
-def myConcat(L): pass
+def myConcat(L):
+    strout = ""
+    for currstr in L:
+        strout += currstr
+    return strout
 
 
 
 ## Problem 10
-def myUnion(L): pass
+def myUnion(L):
+    unionout = set()
+    for currset in L:
+        unionout |= currset
+    return unionout
 
